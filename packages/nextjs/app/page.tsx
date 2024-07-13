3c843acbb4b1501074b4a4387f4201e39510c462
Plauto679 <alberto.alfaro@taiico.com>
2024-07-13 13:33:04 +0200

    web3auth

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,66 +1,117 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import Link from "next/link";
+import { CHAIN_NAMESPACES, IProvider, WEB3AUTH_NETWORK } from "@web3auth/base";
+import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
+import { Web3Auth } from "@web3auth/modal";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
 import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { Address } from "~~/components/scaffold-eth";
+import { useScaffoldReadContract, useScaffoldWriteContract } from "~~/hooks/scaffold-eth";
 
 const Home: NextPage = () => {
   const { address: connectedAddress } = useAccount();
 
+  const { data: totalCounter } = useScaffoldReadContract({
+    contractName: "YourContract",
+    functionName: "count",
+  });
+
+  const { writeContractAsync: writeYourContractAsync } = useScaffoldWriteContract("YourContract");
+
+  const clientId = "BNrUhj1UB5PbmJOSM_LXh6pOWrdTYfMrIB-N4gy8hNt1HDu-exzZLCRPjaeWv1qPtvXPd9rFDHWkiCkmTMKQkb8";
+
+  const [provider, setProvider] = useState<IProvider | null>(null);
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  //Update to the final chain we will deploy
+  const chainConfig = {
+    chainId: "0x1", // Please use 0x1 for Mainnet
+    rpcTarget: "https://rpc.ankr.com/eth",
+    chainNamespace: CHAIN_NAMESPACES.EIP155,
+    displayName: "Ethereum Mainnet",
+    blockExplorerUrl: "https://etherscan.io/",
+    ticker: "ETH",
+    tickerName: "Ethereum",
+    logo: "https://images.toruswallet.io/eth.svg",
+  };
+
+  const privateKeyProvider = new EthereumPrivateKeyProvider({
+    config: { chainConfig: chainConfig },
+  });
+
+  const web3auth = new Web3Auth({
+    clientId,
+    web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_MAINNET,
+    privateKeyProvider: privateKeyProvider,
+  });
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        await web3auth.initModal();
+        setProvider(web3auth.provider);
+
+        if (web3auth.connected) {
+          setLoggedIn(true);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    init();
+  }, []);
+
+  const login = async () => {
+    if (!web3auth) {
+      return;
+    }
+    await web3auth.initModal();
+    const web3authProvider = await web3auth.connect();
+    setProvider(web3authProvider);
+    if (web3auth.connected) {
+      setLoggedIn(true);
+    }
+  };
+
   return (
     <>
-      <div className="flex items-center flex-col flex-grow pt-10">
-        <div className="px-5">
-          <h1 className="text-center">
-            <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
-          </h1>
-          <div className="flex justify-center items-center space-x-2 flex-col sm:flex-row">
-            <p className="my-2 font-medium">Connected Address:</p>
-            <Address address={connectedAddress} />
+      <div className="card card-compact bg-base-100 w-96 shadow-xl">
+        <div className="card-body">
+          <Address address={connectedAddress} />
+          <h2 className="card-title">Shoes!</h2>
+          <p>counter: {totalCounter?.toString()}</p>
+          <div className="card-FFactions justify-end">
+            <button
+              className="btn btn-primary"
+              onClick={async () => {
+                // try {
+                //   await writeYourContractAsync({
+                //     functionName: "createRecord",
+                //     args: [timestamp,mensaje 1],
+                //   });
+                // } catch (e) {
+                //   console.error("Error setting greeting:", e);
+                // }
+              }}
+            >
+              Set Greeting
+            </button>
           </div>
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/app/page.tsx
-            </code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
-          </p>
         </div>
-
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contracts
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
+      </div>
+      <div className="card card-compact bg-base-100 w-96 shadow-xl">
+        <div className="card-body">
+          {/* <Address address={web3AuthAddress} /> */}
+          <h2 className="card-title">Connect</h2>
+          {/* <p>counter: {totalCounter?.toString()}</p> */}
+          <div className="card-FFactions justify-end">
+            <button className="btn btn-primary" onClick={login}>
+              Connect
+            </button>
           </div>
         </div>
       </div>
--- packages/nextjs/contracts/deployedContracts.ts
+++ packages/nextjs/contracts/deployedContracts.ts
@@ -4,6 +4,55 @@
  */
 import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
 
-const deployedContracts = {} as const;
+const deployedContracts = {
+  31337: {
+    YourContract: {
+      address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+      abi: [
+        {
+          inputs: [],
+          name: "count",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [],
+          name: "dec",
+          outputs: [],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+        {
+          inputs: [],
+          name: "get",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [],
+          name: "inc",
+          outputs: [],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+      ],
+      inheritedFunctions: {},
+    },
+  },
+} as const;
 
 export default deployedContracts satisfies GenericContractsDeclaration;
